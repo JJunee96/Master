781d8be2bc55d9b5c6b9c34c8d6fc7cb691d62ce
JJunee <wnsltkak@naver.com>
2022-11-21 23:03:35 +0900

    TEST2

--- TestBasic.c
+++ TestBasic.c
@@ -2,10 +2,15 @@
 int main()
 {
 	int a, b, c;
+
+	printf("첫번째 계산할 항목을 입력하세요 : ");
 	scanf_s("%d", &a);
+	printf("두번째 계산할 항목을 입력하세요 : ");
 	scanf_s("%d", &b);
 	c = a + b;
 	printf("%d + %d = %d", a, b, c);
+
+
 
 	return 0;
 }
